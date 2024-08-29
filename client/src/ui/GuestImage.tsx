bdeac275e1d82b63468ea0328828c8f961eae165
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-29 16:05:36 +0300

    Complete guest image component

--- client/src/ui/GuestImage.tsx
+++ client/src/ui/GuestImage.tsx
@@ -1,10 +1,21 @@
-import { authIllustration, HeaderGuestLayout } from '../constants';
+import { authIllustration, HeaderGuestLayout, logo } from '../constants';
 
 const GuestImage = () => {
   return (
-    <div className="hidden lg:block lg:p-250 lg:h-[100vh] lg:col-span-1">
+    <div
+      className="hidden lg:p-500 lg:col-span-1 lg:flex lg:flex-col lg:justify-between text-white bg-cover bg-no-repeat bg-center"
+      style={{ backgroundImage: `url(${authIllustration})` }}
+    >
       <HeaderGuestLayout />
-      <img src={authIllustration} className="lg:block h-full rounded-[12px]" alt="illustration image" />
+
+      <img src={logo} className="w-32" alt="finance logo" />
+
+      <div className="flex flex-col gap-300">
+        <p className="text-preset-1 font-bold">Keep track of your money and save for your future</p>
+        <p className="text-preset-4">
+          Personal finance app puts you in control of your spending. Track transactions, set budgets, and add to savings pots easily.
+        </p>
+      </div>
     </div>
   );
 };
