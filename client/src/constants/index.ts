fd1433afa44a62a790580782f3ec6586f5ebf233
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-29 14:24:12 +0300

    src/constants/index.ts

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -6,5 +6,32 @@ import Signup from '../features/authentication/Signup';
 import PageNotFound from '../pages/PageNotFound';
 import router from '../router/router';
 import store from '../redux/store';
+import authIllustration from '../assets/images/illustration-authentication.svg';
+import logo from '../assets/images/logo-large.svg';
+import HeaderGuestLayout from '../ui/HeaderGuestLayout';
+import showPasswordIcon from '../assets/images/icon-show-password.svg';
+import hidePasswordIcon from '../assets/images/icon-hide-password.svg';
+import Input from '../ui/Input';
+import Label from '../ui/Label';
+import Button from '../ui/Button';
+import InputField from '../ui/InputField';
 
-export { router, authSlice, GuestLayout, DefaultLayout, Login, Signup, PageNotFound, store };
+export {
+  router,
+  authSlice,
+  GuestLayout,
+  DefaultLayout,
+  Login,
+  Signup,
+  PageNotFound,
+  store,
+  authIllustration,
+  logo,
+  HeaderGuestLayout,
+  Button,
+  showPasswordIcon,
+  hidePasswordIcon,
+  Input,
+  Label,
+  InputField,
+};
