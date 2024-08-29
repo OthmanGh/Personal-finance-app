7541effaaaf6f258d3d533ba7a7d32d2ef4f3ec4
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-29 03:17:41 +0300

    Add app primary colors to tailwind config file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          beige: '#98908B',
+          'beige-light': '#F8F4F0',
+          'grey-900': '#201F24',
+          'grey-500': '#696868',
+          'grey-100': '#F2F2F2',
+        },
+      },
+    },
   },
   plugins: [],
 };
