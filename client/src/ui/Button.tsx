90f964b1a75ecedffe7cf91c978237479b82c941
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-29 14:23:34 +0300

    Add button type

--- client/src/ui/Button.tsx
+++ client/src/ui/Button.tsx
@@ -2,12 +2,13 @@ import { ReactNode } from 'react';
 
 type ButtonProps = {
   children: ReactNode;
+  type?: 'button' | 'reset' | 'submit';
   onClick?: () => void;
 };
 
-const Button = ({ children, onClick }: ButtonProps) => {
+const Button = ({ children, type = 'button', onClick }: ButtonProps) => {
   return (
-    <button onClick={onClick} className="bg-primary-grey-900 text-white rounded-[8px] w-[303px] h-[53px]">
+    <button onClick={onClick} type={type} className="bg-primary-grey-900 text-white rounded-[8px] w-[303px] h-[53px]">
       {children}
     </button>
   );
