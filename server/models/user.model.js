3dfbdc3e07884bab846b86c41ad0adc07d5db8bc
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-30 11:04:37 +0300

    Hash password before saving to database

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
@@ -31,11 +32,17 @@ const userSchema = new mongoose.Schema(
       default: 'user',
     },
   },
-
   {
     timestamps: true,
   }
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
