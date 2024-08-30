dc7e3f1b26612addcf1f126e04d295363a2f02df
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-30 20:44:59 +0300

    Toggle show & hide password

--- client/src/ui/InputField.tsx
+++ client/src/ui/InputField.tsx
@@ -1,22 +1,35 @@
-import { showPasswordIcon } from '../constants';
+import { UseFormRegister } from 'react-hook-form';
+import { hidePasswordIcon, showPasswordIcon } from '../constants';
 import Input from './Input';
+import { useState } from 'react';
 
 type InputFieldProps = {
   type: string;
   id: string;
   isPassword?: boolean;
   passwordTerms?: boolean;
+  register?: UseFormRegister<any>;
   onChange?: () => void;
+  validation?: object;
 };
 
-const InputField = ({ type, id, isPassword = false, passwordTerms = false, onChange }: InputFieldProps) => {
+const InputField = ({ type, id, isPassword = false, onChange, register, validation }: InputFieldProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const inputType = isPassword ? (showPassword ? 'text' : 'password') : type;
+
   return (
     <div>
       <div className="border-[1px] border-primary-grey-500 rounded-[8px] w-full px-250 py-150 relative">
-        <Input type={type} id={id} onChange={onChange} />
-        {isPassword && <img src={showPasswordIcon} alt="eye password icon" className="absolute right-3 top-5" />}
+        <Input type={inputType} id={id} onChange={onChange} register={register} validation={validation} />
+        {isPassword && (
+          <img
+            src={showPassword ? showPasswordIcon : hidePasswordIcon}
+            alt="eye password icon"
+            className="absolute right-3 top-5 cursor-pointer"
+            onClick={() => setShowPassword((prev) => !prev)}
+          />
+        )}
       </div>
-      {passwordTerms && <p className="text-preset-5 text-end mt-50">Passwords must be at least 8 characters</p>}
     </div>
   );
 };
