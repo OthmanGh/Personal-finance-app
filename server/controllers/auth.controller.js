b1b39f8e6d81237a34f125e398491a0f163ee766
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-30 11:20:59 +0300

    Implement login functionality

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -33,8 +33,33 @@ export const signup = async (req, res, next) => {
   }
 };
 
-export const login = (req, res) => {
-  res.status(200).json({
-    messaege: 'Hello from login',
-  });
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      throw new Error('Email and password are required.');
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.isPasswordCorrect(password, user.password))) {
+      throw new Error('Incorrect email or password');
+    }
+
+    const token = signToken(user._id);
+
+    res.status(200).json({
+      status: 'success',
+      token,
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    res.status(401).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
 };
