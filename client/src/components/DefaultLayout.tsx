4fc47d6ff9849ce4d1546033997f6fbb2a0de909
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-31 23:47:28 +0300

    Handle toggle sidebar

--- client/src/components/DefaultLayout.tsx
+++ client/src/components/DefaultLayout.tsx
@@ -1,15 +1,24 @@
 import { Outlet, Navigate } from 'react-router-dom';
-import { Sidebar } from '../constants';
+import { MinimizeSidebar, Sidebar } from '../constants';
+import { useState } from 'react';
 
 const DefaultLayout = () => {
-  const token = localStorage.getItem('ACCESS_TOKEN');
+  const [isMinimize, setIsMinimize] = useState(false);
 
+  const token = localStorage.getItem('ACCESS_TOKEN');
   if (!token) return <Navigate to="/login" />;
 
+  const toggleSidebar = () => {
+    setIsMinimize((prev) => !prev);
+  };
+
   return (
-    <div className="flex flex-col-reverse lg:flex-row min-h-screen bg-primary-beige-light">
-      <Sidebar />
-      <Outlet />
+    <div className="flex flex-col-reverse lg:flex-row h-screen bg-primary-beige-light transition-all duration-400">
+      {!isMinimize ? <Sidebar onToggle={toggleSidebar} /> : <MinimizeSidebar onToggle={toggleSidebar} />}
+
+      <div className="px-200 py-300 sm:px-400 sm:py-500">
+        <Outlet />
+      </div>
     </div>
   );
 };
