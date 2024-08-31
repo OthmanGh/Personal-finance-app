85460cdf5a6ec23d03c5b2d6ee9492805ed269ca
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-31 11:43:02 +0300

    Create use register custom hook for user signup/login

---
+++ client/src/hooks/useRegister.ts
@@ -0,0 +1,43 @@
+import { useForm } from 'react-hook-form';
+import { signupSchema, loginSchema, api } from '../constants';
+import { useNavigate } from 'react-router-dom';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useDispatch } from 'react-redux';
+import { setToken } from '../features/authentication/authSlice';
+
+const useRegister = (isSignup: boolean) => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(isSignup ? signupSchema : loginSchema),
+  });
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onSubmit = async (data: any) => {
+    try {
+      const endpoint = isSignup ? 'signup' : 'login';
+
+      const res = await api.post(endpoint, data);
+
+      if (res.data.status === 'success') {
+        dispatch(setToken(res.data.token));
+        navigate('/overview');
+      }
+    } catch (error: any) {
+      const backendResponse = error.response?.data?.message || 'Something went wrogn';
+
+      setError('root', {
+        message: backendResponse,
+      });
+    }
+  };
+
+  return { onSubmit, register, handleSubmit, errors, isSubmitting };
+};
+
+export default useRegister;
