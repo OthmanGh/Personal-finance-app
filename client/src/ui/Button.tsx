d4d7cfcfe06985ade374d4a183e92867b326055f
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-31 11:44:57 +0300

    Modify button styles & functionality

--- client/src/ui/Button.tsx
+++ client/src/ui/Button.tsx
@@ -3,13 +3,19 @@ import { ReactNode } from 'react';
 type ButtonProps = {
   children: ReactNode;
   type?: 'button' | 'reset' | 'submit';
+  isSubmitting?: boolean;
   onClick?: () => void;
 };
 
-const Button = ({ children, type = 'button', onClick }: ButtonProps) => {
+const Button = ({ children, type = 'button', isSubmitting, onClick }: ButtonProps) => {
   return (
-    <button onClick={onClick} type={type} className="bg-primary-grey-900 text-white rounded-[8px] h-[53px]">
-      {children}
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={isSubmitting}
+      className="bg-primary-grey-900 transition-all duration-300 text-white border-2 border-primary-grey-900 rounded-[8px] h-[53px] hover:bg-primary-grey-500 hover:border-primary-grey-500 disabled:bg-primary-beige disabled:border-primary-beige"
+    >
+      {isSubmitting ? 'Loading...' : children}
     </button>
   );
 };
