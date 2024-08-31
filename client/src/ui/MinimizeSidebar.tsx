dd8cfdb7f0d7477cd771d8b474ea4296fb541eb9
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-31 23:46:37 +0300

    Create & style minimize sidebar

---
+++ client/src/ui/MinimizeSidebar.tsx
@@ -0,0 +1,30 @@
+import { sidebarContent, smallLogo, vector } from '../constants';
+import { NavLink } from 'react-router-dom';
+
+type MinimizeSidebarProps = {
+  onToggle: () => void;
+};
+
+const MinimizeSidebar = ({ onToggle }: MinimizeSidebarProps) => {
+  return (
+    <aside className="h-screen bg-primary-grey-900 w-[88px] rounded-r-[16px] flex flex-col items-center justify-between pb-400">
+      <div className="flex flex-col py-500 px-300 gap-300">
+        <div className="flex items-center justify-center h-[101.44px]">
+          <img src={smallLogo} alt="f logo" className="" />
+        </div>
+
+        <ul>
+          {sidebarContent.map((item) => (
+            <NavLink to={item.to}>
+              <img src={item.src} key={item.id} className="py-300" />
+            </NavLink>
+          ))}
+        </ul>
+      </div>
+
+      <img src={vector} alt="expand sidebar logo" className="cursor-pointer" onClick={onToggle} />
+    </aside>
+  );
+};
+
+export default MinimizeSidebar;
