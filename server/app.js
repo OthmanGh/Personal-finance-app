868b4e6634945e18c09002c3b48eec0b95d3605a
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-31 11:47:59 +0300

    Install & integrate cors to allow access origin

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import authRoute from './routes/auth.routes.js';
 const app = express();
+import cors from 'cors';
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+  })
+);
 
-app.use('/api/v1/auth', authRoute);
+app.use('/api/v1', authRoute);
 
 export default app;
