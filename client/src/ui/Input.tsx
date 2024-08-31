15e9d1e974f663b9111dd97c31954bc4441335e5
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-31 11:46:07 +0300

    Modify input fields styles & integrate react hook form

--- client/src/ui/Input.tsx
+++ client/src/ui/Input.tsx
@@ -1,11 +1,14 @@
+import { UseFormRegister } from 'react-hook-form';
+
 type inputProps = {
   type: string;
   id: string;
-  onChange?: () => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  register: UseFormRegister<any>;
 };
 
-const Input = ({ type, id, onChange }: inputProps) => {
-  return <input className="border-none outline-none focus:outline-none w-[100%] font-bold" type={type} id={id} onChange={onChange} />;
+const Input = ({ type, id, onChange, register }: inputProps) => {
+  return <input className="border-none outline-none focus:outline-none w-[100%] font-bold" type={type} id={id} {...register(id)} />;
 };
 
 export default Input;
--- client/src/ui/InputField.tsx
+++ client/src/ui/InputField.tsx
@@ -10,17 +10,16 @@ type InputFieldProps = {
   passwordTerms?: boolean;
   register?: UseFormRegister<any>;
   onChange?: () => void;
-  validation?: object;
 };
 
-const InputField = ({ type, id, isPassword = false, onChange, register, validation }: InputFieldProps) => {
+const InputField = ({ type, id, isPassword = false, onChange, register }: InputFieldProps) => {
   const [showPassword, setShowPassword] = useState(false);
   const inputType = isPassword ? (showPassword ? 'text' : 'password') : type;
 
   return (
     <div>
-      <div className="border-[1px] border-primary-grey-500 rounded-[8px] w-full px-250 py-150 relative">
-        <Input type={inputType} id={id} onChange={onChange} register={register} validation={validation} />
+      <div className="border-[1px] border-primary-grey-500 border-opacity-60 focus-within:border-primary-grey-900 rounded-[8px] w-full pr-400 pl-250 py-150 relative">
+        <Input type={inputType} id={id} onChange={onChange} register={register} />
         {isPassword && (
           <img
             src={showPassword ? showPasswordIcon : hidePasswordIcon}
