6f77c90eeafd0d6360f0f3e1c820a6e9493e0520
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-01 23:13:48 +0300

    Update constants imports & exports

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -5,7 +5,8 @@ import Budgets from '../pages/Budgets';
 import Transactions from '../pages/Transactions';
 import Pots from '../pages/Pots';
 import RecurringBills from '../pages/RecurringBills';
-
+import MinimizeMenu from '../ui/MinimizeMenu';
+import MinimizeSidebar from '../ui/MinimizeSidebar';
 import authSlice from '../features/authentication/authSlice';
 import Login from '../features/authentication/Login';
 import Signup from '../features/authentication/Signup';
@@ -31,7 +32,18 @@ import iconNavBudgets from '../assets/images/icon-nav-budgets.svg';
 import iconNavPots from '../assets/images/icon-nav-pots.svg';
 import iconNavRecurringBills from '../assets/images/icon-nav-recurring-bills.svg';
 import iconsMinimizeMenu from '../assets/images/icon-minimize-menu.svg';
-
+import smallLogo from '../assets/images/logo-small.svg';
+import vector from '../assets/images/Vector.svg';
+import HeaderDefaultLayout from '../ui/HeaderDefaultLayout';
+import MoneyCard from '../ui/MoneyCard';
+import OverviewContentLayout from '../ui/OverviewContentLayout';
+import CardsOverviewContainer from '../ui/CardsOverviewContainer';
+import PotsOverview from '../features/overview/PotsOverview';
+import iconCaretRight from '../assets/images/icon-caret-right.svg';
+import iconPot from '../assets/images/icon-pot.svg';
+import TransactionsOverview from '../features/overview/TransactionsOverview';
+import UsersTransactionsContainer from '../features/overview/UsersTransactionsContainer';
+import { formatDate } from '../utils/helper';
 const sidebarContent = [
   {
     id: 1,
@@ -69,6 +81,24 @@ const sidebarContent = [
   },
 ];
 
+const secondaryColors = {
+  green: 'border-secondary-green',
+  yellow: 'border-secondary-yellow',
+  cyan: 'border-secondary-cyan',
+  navy: 'border-secondary-navy',
+  red: 'border-secondary-red',
+  purple: 'border-secondary-purple',
+  'purple-light': 'border-secondary-purple-light',
+  turquoise: 'border-secondary-turquoise',
+  brown: 'border-secondary-brown',
+  magenta: 'border-secondary-magenta',
+  blue: 'border-secondary-blue',
+  'navy-grey': 'border-secondary-navy-grey',
+  'army-green': 'border-secondary-army-green',
+  gold: 'border-secondary-gold',
+  orange: 'border-secondary-orange',
+};
+
 export {
   router,
   authSlice,
@@ -99,4 +129,19 @@ export {
   RecurringBills,
   Pots,
   iconsMinimizeMenu,
+  MinimizeSidebar,
+  MinimizeMenu,
+  smallLogo,
+  vector,
+  HeaderDefaultLayout,
+  MoneyCard,
+  OverviewContentLayout,
+  CardsOverviewContainer,
+  PotsOverview,
+  iconCaretRight,
+  iconPot,
+  secondaryColors,
+  TransactionsOverview,
+  UsersTransactionsContainer,
+  formatDate,
 };
