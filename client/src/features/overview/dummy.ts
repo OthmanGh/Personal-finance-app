b9267dc972cf5ef69a3715819b2606229e0aab79
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-01 23:04:51 +0300

    Add dummy users transactions data for testing

---
+++ client/src/features/overview/dummy.ts
@@ -0,0 +1,75 @@
+import emmaAvatar from '../../assets/images/avatars/emma-richardson.jpg';
+import savoryBitesBistroAvatar from '../../assets/images/avatars/savory-bites-bistro.jpg';
+import danielCarterAvatar from '../../assets/images/avatars/daniel-carter.jpg';
+import sunParkAvatar from '../../assets/images/avatars/sun-park.jpg';
+import urbanServicesHubAvatar from '../../assets/images/avatars/urban-services-hub.jpg';
+import liamHughesAvatar from '../../assets/images/avatars/liam-hughes.jpg';
+import lilyRamirezAvatar from '../../assets/images/avatars/lily-ramirez.jpg';
+import ethanClarkAvatar from '../../assets/images/avatars/ethan-clark.jpg';
+
+export const transactions = [
+  {
+    avatar: emmaAvatar,
+    name: 'Emma Richardson',
+    category: 'General',
+    date: '2024-08-19T14:23:11Z',
+    amount: 75.5,
+    recurring: false,
+  },
+  {
+    avatar: savoryBitesBistroAvatar,
+    name: 'Savory Bites Bistro',
+    category: 'Dining Out',
+    date: '2024-08-19T20:23:11Z',
+    amount: -55.5,
+    recurring: false,
+  },
+  {
+    avatar: danielCarterAvatar,
+    name: 'Daniel Carter',
+    category: 'General',
+    date: '2024-08-18T09:45:32Z',
+    amount: -42.3,
+    recurring: false,
+  },
+  {
+    avatar: sunParkAvatar,
+    name: 'Sun Park',
+    category: 'General',
+    date: '2024-08-17T16:12:05Z',
+    amount: 120.0,
+    recurring: false,
+  },
+  {
+    avatar: urbanServicesHubAvatar,
+    name: 'Urban Services Hub',
+    category: 'General',
+    date: '2024-08-17T21:08:09Z',
+    amount: -65.0,
+    recurring: false,
+  },
+  {
+    avatar: liamHughesAvatar,
+    name: 'Liam Hughes',
+    category: 'Groceries',
+    date: '2024-08-15T18:20:33Z',
+    amount: 65.75,
+    recurring: false,
+  },
+  {
+    avatar: lilyRamirezAvatar,
+    name: 'Lily Ramirez',
+    category: 'General',
+    date: '2024-08-14T13:05:27Z',
+    amount: 50.0,
+    recurring: false,
+  },
+  {
+    avatar: ethanClarkAvatar,
+    name: 'Ethan Clark',
+    category: 'Dining Out',
+    date: '2024-08-13T20:15:59Z',
+    amount: -32.5,
+    recurring: false,
+  },
+];
