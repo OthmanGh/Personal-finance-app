9988e6ef7c812b7d1d303a0ed2575f9a6fe520a2
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-01 23:13:15 +0300

    Modify default layout styles

--- client/src/components/DefaultLayout.tsx
+++ client/src/components/DefaultLayout.tsx
@@ -13,10 +13,10 @@ const DefaultLayout = () => {
   };
 
   return (
-    <div className="flex flex-col-reverse lg:flex-row h-screen bg-primary-beige-light transition-all duration-400">
+    <div className="flex flex-col-reverse lg:flex-row min-h-screen bg-primary-beige-light transition-all duration-400">
       {!isMinimize ? <Sidebar onToggle={toggleSidebar} /> : <MinimizeSidebar onToggle={toggleSidebar} />}
 
-      <div className="px-200 py-300 sm:px-400 sm:py-500">
+      <div className="px-200 py-300 sm:px-400 sm:py-500 w-full flex-1">
         <Outlet />
       </div>
     </div>
