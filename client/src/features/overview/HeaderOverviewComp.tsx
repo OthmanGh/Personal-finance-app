30f4d94d34b8366aed65bc3fce18be6dafbbb2fb
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-02 14:53:10 +0300

    Add hover effect on see details text

--- client/src/features/overview/HeaderOverviewComp.tsx
+++ client/src/features/overview/HeaderOverviewComp.tsx
@@ -13,7 +13,9 @@ const HeaderOverviewComp = ({ title, to }: HeaderOverviewCompProps) => {
     <div className="flex items-center justify-between ">
       <h2 className="text-preset-2">{title}</h2>
       <div className="flex items-center justify-center gap-150 cursor-pointer" onClick={() => navigate(`/${to}`)}>
-        <p className="text-preset-4 text-primary-grey-500 font-light">See Details</p>
+        <p className="text-preset-4 text-primary-grey-500 font-light hover:text-primary-grey-900 transition-all duration-300 hover:font-semibold">
+          See Details
+        </p>
         <img src={iconCaretRight} alt="caret left icon" />
       </div>
     </div>
