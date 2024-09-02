edc427d94a0937272182c24cd7cbe83a0064c31b
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-02 14:20:04 +0300

    Handle sidebar switching on tables & small devices

--- client/src/components/DefaultLayout.tsx
+++ client/src/components/DefaultLayout.tsx
@@ -1,20 +1,34 @@
 import { Outlet, Navigate } from 'react-router-dom';
 import { MinimizeSidebar, Sidebar } from '../constants';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const DefaultLayout = () => {
   const [isMinimize, setIsMinimize] = useState(false);
 
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const token = localStorage.getItem('ACCESS_TOKEN');
   if (!token) return <Navigate to="/login" />;
 
-  const toggleSidebar = () => {
+  const handleToggleSidebar = () => {
     setIsMinimize((prev) => !prev);
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth < 1024) {
+      setIsMinimize(false);
+    }
   };
 
   return (
     <div className="flex flex-col-reverse lg:flex-row min-h-screen bg-primary-beige-light transition-all duration-400">
-      {!isMinimize ? <Sidebar onToggle={toggleSidebar} /> : <MinimizeSidebar onToggle={toggleSidebar} />}
+      {!isMinimize ? <Sidebar onToggle={handleToggleSidebar} /> : <MinimizeSidebar onToggle={handleToggleSidebar} />}
 
       <div className="px-200 py-300 sm:px-400 sm:py-500 w-full flex-1">
         <Outlet />
