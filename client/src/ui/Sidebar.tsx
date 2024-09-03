698460613ade39ac54c67b1adecbfafacecd1051
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-03 23:53:52 +0300

    Modify sidebar styles

--- client/src/ui/Sidebar.tsx
+++ client/src/ui/Sidebar.tsx
@@ -8,7 +8,7 @@ type SidebarProps = {
 
 const Sidebar = ({ onToggle }: SidebarProps) => {
   return (
-    <aside className="w-full  sticky bottom-0 rounded-t-xl bg-primary-grey-900 text-white lg:min-h-screen overflow-hidden lg:flex lg:flex-col lg:w-[300px] lg:justify-between lg:rounded-t-none lg:rounded-e-xl transition-all duration-400">
+    <aside className="w-full sticky bottom-0 rounded-t-xl bg-primary-grey-900 text-white lg:min-h-screen overflow-hidden lg:flex lg:flex-col lg:w-[300px] lg:justify-between lg:rounded-t-none lg:rounded-e-xl transition-all duration-400">
       <div>
         <img src={logo} alt="finance logo" className="hidden lg:block px-400 py-500" />
         <NavSidebar />
