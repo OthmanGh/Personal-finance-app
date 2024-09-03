a00511c75841437177c726c6f6f2b2966437cb13
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-03 23:58:41 +0300

    Add dummy test transactions data

---
+++ client/src/features/transactions/dummy.ts
@@ -0,0 +1,273 @@
+import emmaIcon from '../../assets/images/avatars/emma-richardson.jpg';
+import savoryBitesBistroIcon from '../../assets/images/avatars/savory-bites-bistro.jpg';
+import danielCarterIcon from '../../assets/images/avatars/daniel-carter.jpg';
+import sunParkIcon from '../../assets/images/avatars/sun-park.jpg';
+import urbanServicesHubIcon from '../../assets/images/avatars/urban-services-hub.jpg';
+import liamHughesIcon from '../../assets/images/avatars/liam-hughes.jpg';
+import lilyRamirezIcon from '../../assets/images/avatars/lily-ramirez.jpg';
+import ethanClarkIcon from '../../assets/images/avatars/ethan-clark.jpg';
+import jamesThompsonIcon from '../../assets/images/avatars/james-thompson.jpg';
+import pixelPlaygroundIcon from '../../assets/images/avatars/pixel-playground.jpg';
+import ellaPhillipsIcon from '../../assets/images/avatars/ella-phillips.jpg';
+import sofiaPetersonIcon from '../../assets/images/avatars/sofia-peterson.jpg';
+import masonMartinezIcon from '../../assets/images/avatars/mason-martinez.jpg';
+import greenPlateEateryIcon from '../../assets/images/avatars/green-plate-eatery.jpg';
+import sebastianCookIcon from '../../assets/images/avatars/sebastian-cook.jpg';
+import williamHarrisIcon from '../../assets/images/avatars/william-harris.jpg';
+import elevateEducationIcon from '../../assets/images/avatars/elevate-education.jpg';
+import serenitySpaIcon from '../../assets/images/avatars/serenity-spa-and-wellness.jpg';
+import sparkElectricIcon from '../../assets/images/avatars/spark-electric-solutions.jpg';
+import rinaSatoIcon from '../../assets/images/avatars/rina-sato.jpg';
+import swiftRideShareIcon from '../../assets/images/avatars/swift-ride-share.jpg';
+import aquaFlowUtilitiesIcon from '../../assets/images/avatars/aqua-flow-utilities.jpg';
+import ecoFuelEnergyIcon from '../../assets/images/avatars/ecofuel-energy.jpg';
+import yunaKimIcon from '../../assets/images/avatars/yuna-kim.jpg';
+import flavorFiestaIcon from '../../assets/images/avatars/flavor-fiesta.jpg';
+import harperEdwardsIcon from '../../assets/images/avatars/harper-edwards.jpg';
+import buzzMarketingGroupIcon from '../../assets/images/avatars/buzz-marketing-group.jpg';
+import techNovaInnovationsIcon from '../../assets/images/avatars/technova-innovations.jpg';
+import byteWiseIcon from '../../assets/images/avatars/bytewise.jpg';
+import nimbusDataStorageIcon from '../../assets/images/avatars/nimbus-data-storage.jpg';
+
+export const transactions = [
+  {
+    avatar: emmaIcon,
+    name: 'Emma Richardson',
+    category: 'General',
+    date: '2024-08-19T14:23:11Z',
+    amount: 75.5,
+    recurring: false,
+  },
+  {
+    avatar: savoryBitesBistroIcon,
+    name: 'Savory Bites Bistro',
+    category: 'Dining Out',
+    date: '2024-08-19T20:23:11Z',
+    amount: -55.5,
+    recurring: false,
+  },
+  {
+    avatar: danielCarterIcon,
+    name: 'Daniel Carter',
+    category: 'General',
+    date: '2024-08-18T09:45:32Z',
+    amount: -42.3,
+    recurring: false,
+  },
+  {
+    avatar: sunParkIcon,
+    name: 'Sun Park',
+    category: 'General',
+    date: '2024-08-17T16:12:05Z',
+    amount: 120.0,
+    recurring: false,
+  },
+  {
+    avatar: urbanServicesHubIcon,
+    name: 'Urban Services Hub',
+    category: 'General',
+    date: '2024-08-17T21:08:09Z',
+    amount: -65.0,
+    recurring: false,
+  },
+  {
+    avatar: liamHughesIcon,
+    name: 'Liam Hughes',
+    category: 'Groceries',
+    date: '2024-08-15T18:20:33Z',
+    amount: 65.75,
+    recurring: false,
+  },
+  {
+    avatar: lilyRamirezIcon,
+    name: 'Lily Ramirez',
+    category: 'General',
+    date: '2024-08-14T13:05:27Z',
+    amount: 50.0,
+    recurring: false,
+  },
+  {
+    avatar: ethanClarkIcon,
+    name: 'Ethan Clark',
+    category: 'Dining Out',
+    date: '2024-08-13T20:15:59Z',
+    amount: -32.5,
+    recurring: false,
+  },
+  {
+    avatar: jamesThompsonIcon,
+    name: 'James Thompson',
+    category: 'Entertainment',
+    date: '2024-08-11T15:45:38Z',
+    amount: -5.0,
+    recurring: false,
+  },
+  {
+    avatar: pixelPlaygroundIcon,
+    name: 'Pixel Playground',
+    category: 'Entertainment',
+    date: '2024-08-11T18:45:38Z',
+    amount: -10.0,
+    recurring: true,
+  },
+  {
+    avatar: ellaPhillipsIcon,
+    name: 'Ella Phillips',
+    category: 'Dining Out',
+    date: '2024-08-10T19:22:51Z',
+    amount: -45.0,
+    recurring: false,
+  },
+  {
+    avatar: sofiaPetersonIcon,
+    name: 'Sofia Peterson',
+    category: 'Transportation',
+    date: '2024-08-08T08:55:17Z',
+    amount: -15.0,
+    recurring: false,
+  },
+  {
+    avatar: masonMartinezIcon,
+    name: 'Mason Martinez',
+    category: 'Lifestyle',
+    date: '2024-08-07T17:40:29Z',
+    amount: -35.25,
+    recurring: false,
+  },
+  {
+    avatar: greenPlateEateryIcon,
+    name: 'Green Plate Eatery',
+    category: 'Groceries',
+    date: '2024-08-06T08:25:44Z',
+    amount: -78.5,
+    recurring: false,
+  },
+  {
+    avatar: sebastianCookIcon,
+    name: 'Sebastian Cook',
+    category: 'Transportation',
+    date: '2024-08-06T10:05:44Z',
+    amount: -22.5,
+    recurring: false,
+  },
+  {
+    avatar: williamHarrisIcon,
+    name: 'William Harris',
+    category: 'Personal Care',
+    date: '2024-08-05T14:30:56Z',
+    amount: -10.0,
+    recurring: false,
+  },
+  {
+    avatar: elevateEducationIcon,
+    name: 'Elevate Education',
+    category: 'Education',
+    date: '2024-08-04T11:15:22Z',
+    amount: -50.0,
+    recurring: true,
+  },
+  {
+    avatar: serenitySpaIcon,
+    name: 'Serenity Spa & Wellness',
+    category: 'Personal Care',
+    date: '2024-08-03T14:00:37Z',
+    amount: -30.0,
+    recurring: true,
+  },
+  {
+    avatar: sparkElectricIcon,
+    name: 'Spark Electric Solutions',
+    category: 'Bills',
+    date: '2024-08-02T09:25:11Z',
+    amount: -100.0,
+    recurring: true,
+  },
+  {
+    avatar: rinaSatoIcon,
+    name: 'Rina Sato',
+    category: 'Bills',
+    date: '2024-08-02T13:31:11Z',
+    amount: -50.0,
+    recurring: false,
+  },
+  {
+    avatar: swiftRideShareIcon,
+    name: 'Swift Ride Share',
+    category: 'Transportation',
+    date: '2024-08-01T18:40:33Z',
+    amount: -18.75,
+    recurring: false,
+  },
+  {
+    avatar: aquaFlowUtilitiesIcon,
+    name: 'Aqua Flow Utilities',
+    category: 'Bills',
+    date: '2024-07-30T13:20:14Z',
+    amount: -100.0,
+    recurring: true,
+  },
+  {
+    avatar: ecoFuelEnergyIcon,
+    name: 'EcoFuel Energy',
+    category: 'Bills',
+    date: '2024-07-29T11:55:29Z',
+    amount: -35.0,
+    recurring: true,
+  },
+  {
+    avatar: yunaKimIcon,
+    name: 'Yuna Kim',
+    category: 'Dining Out',
+    date: '2024-07-29T13:51:29Z',
+    amount: -28.5,
+    recurring: false,
+  },
+  {
+    avatar: flavorFiestaIcon,
+    name: 'Flavor Fiesta',
+    category: 'Dining Out',
+    date: '2024-07-27T20:15:06Z',
+    amount: -42.75,
+    recurring: false,
+  },
+  {
+    avatar: harperEdwardsIcon,
+    name: 'Harper Edwards',
+    category: 'Groceries',
+    date: '2024-07-26T15:30:27Z',
+    amount: 60.0,
+    recurring: false,
+  },
+  {
+    avatar: buzzMarketingGroupIcon,
+    name: 'Buzz Marketing Group',
+    category: 'Business',
+    date: '2024-07-25T10:55:12Z',
+    amount: 150.0,
+    recurring: false,
+  },
+  {
+    avatar: techNovaInnovationsIcon,
+    name: 'TechNova Innovations',
+    category: 'Business',
+    date: '2024-07-25T12:10:18Z',
+    amount: 250.0,
+    recurring: false,
+  },
+  {
+    avatar: byteWiseIcon,
+    name: 'ByteWise',
+    category: 'Business',
+    date: '2024-07-24T11:25:35Z',
+    amount: 200.0,
+    recurring: false,
+  },
+  {
+    avatar: nimbusDataStorageIcon,
+    name: 'Nimbus Data Storage',
+    category: 'Business',
+    date: '2024-07-23T09:00:47Z',
+    amount: 300.0,
+    recurring: false,
+  },
+];
