58c72fbc12f50b78949931b8e7cddf9081c2f110
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-04 19:38:26 +0300

    Create recurring bills component

--- client/src/pages/RecurringBills.tsx
+++ client/src/pages/RecurringBills.tsx
@@ -1,5 +1,15 @@
+import { HeaderDefaultLayout, BillsStats, BillsTable } from '../constants';
+
 const RecurringBills = () => {
-  return <div>Recurring Bills</div>;
+  return (
+    <div className="flex flex-col gap-300">
+      <div>
+        <HeaderDefaultLayout>Recurring Bills</HeaderDefaultLayout>
+        <BillsStats />
+      </div>
+      <BillsTable />
+    </div>
+  );
 };
 
 export default RecurringBills;
