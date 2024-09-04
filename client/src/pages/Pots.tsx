a17b97e034907fb2200109bc8d5d7a8384616c3e
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-04 19:43:37 +0300

    Modify pots page component

--- client/src/pages/Pots.tsx
+++ client/src/pages/Pots.tsx
@@ -1,5 +1,15 @@
+import { Button, HeaderDefaultLayout, PotsContainer } from '../constants';
 const Pots = () => {
-  return <div>Pots</div>;
+  return (
+    <div className="flex flex-col gap-400">
+      <div className="flex flex-row items-center justify-center">
+        <HeaderDefaultLayout>Pots</HeaderDefaultLayout>
+        <Button type="button">+ Add New Budget</Button>
+      </div>
+
+      <PotsContainer />
+    </div>
+  );
 };
 
 export default Pots;
