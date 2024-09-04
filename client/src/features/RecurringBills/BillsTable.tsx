2aefd54292c2454b61ffdba48823b0e7feb41270
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-04 19:31:58 +0300

    Create bills table component

---
+++ client/src/features/RecurringBills/BillsTable.tsx
@@ -0,0 +1,11 @@
+import BillsTableHeader from './BillsTableHeader';
+
+const BillsTable = () => {
+  return (
+    <div className="flex flex-col gap-300 bg-white px-250 py-300 rounded-lg">
+      <BillsTableHeader />
+    </div>
+  );
+};
+
+export default BillsTable;
